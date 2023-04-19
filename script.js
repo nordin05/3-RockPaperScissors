afb5c319bfe6b962739ebaeb0854de0cdc764192
Nordin <128930417+nordin05@users.noreply.github.com>
2023-04-19 11:51:21 +0200

    Added getUserInput and checkUserInput
    Get input from the user, convert input to first letter upprcase string
    and then compare with the valid choices. If the input is not valid, ask
    again (only once for now).

--- script.js
+++ script.js
@@ -4,29 +4,57 @@ function getComputerChoice(){
     return possibleChoices[Math.floor(Math.random()*possibleChoices.length)];
 }
 
+function checkUserInput(userInput){
+    userInput = userInput.toLowerCase();
+    userInput = userInput.charAt(0).toUpperCase() + userInput.slice(1);
+
+    if (possibleChoices.includes(userInput)){
+        return [true, userInput];
+    }
+    else{
+        return [false, userInput];
+    }
+}
+
+function getUserInput(){
+    userInput = prompt("Choose Rock, Paper or Scissors");
+    let inputArray = checkUserInput(userInput);
+
+    return inputArray;
+}
+
+
 function startRound(playerChoice, computerChoice){
     if ((playerChoice == "Rock" && computerChoice == "Scissors")
         || (playerChoice == "Paper" && computerChoice == "Rock")
         || (playerChoice == "Scissors" && computerChoice == "Paper")) {
-        return "Won";
+        return "You Win! " + playerChoice + " beats " + computerChoice;
     }
     
     if ((playerChoice == "Paper" && computerChoice == "Scissors")
         || (playerChoice == "Scissors" && computerChoice == "Rock")
         || (playerChoice == "Rock" && computerChoice == "Paper")) {
-        return "Lost";
+            return "You Lose! " + computerChoice + " beats " + playerChoice;
     }
 
     else{
-        return "Tied";
+        return "You tied!";
     }
 }
 
-let playerChoice = getComputerChoice();
+let inputArray = getUserInput();
+if (inputArray[0] == false){
+    console.log("Please choose Rock, Paper or Scissors");
+    inputArray = getUserInput();
+}
+else {
+
+}
+
 let computerChoice = getComputerChoice();
 
-console.log("User: " + playerChoice);
+console.log("User: " + inputArray[1]);
 console.log("Computer: " + computerChoice);
 
-let result = startRound(playerChoice,computerChoice)
-console.log("Result: " + result);
+let result = startRound(inputArray[1], computerChoice)
+console.log("You " + result);
