21ebda392b149feaf3721a208838391f62779f05
Nordin <128930417+nordin05@users.noreply.github.com>
2023-04-26 13:30:15 +0200

    Added button press and text updating functionality

--- script.js
+++ script.js
@@ -1,83 +1,70 @@
 const possibleChoices = ["Rock", "Paper", "Scissors"];
 const rounds = 5;
-let score = 0;
+let scoreUser = 0;
+let scoreComp = 0;
 
 function getComputerChoice(){
     return possibleChoices[Math.floor(Math.random()*possibleChoices.length)];
-}
-
-function checkUserInput(userInput){
-    userInput = userInput.toLowerCase();
-    userInput = userInput.charAt(0).toUpperCase() + userInput.slice(1);
-
-    if (possibleChoices.includes(userInput)){
-        return [true, userInput];
-    }
-    else{
-        return [false, userInput];
-    }
-}
-
-function getUserInput(){
-    userInput = prompt("Choose Rock, Paper or Scissors");
-    let inputArray = checkUserInput(userInput);
-
-    return inputArray;
-}
-
-function printResult(result, playerChoice, computerChoice){
-    if (result == 1){
-        console.log("You Win! " + playerChoice + " beats " + computerChoice);
-    }
-    else if (result == -1){
-        console.log("You Lose! " + computerChoice + " beats " + playerChoice);
-    }
-    else{
-        console.log("You tied!");
-    }
 }
 
 function startRound(playerChoice, computerChoice){
     if ((playerChoice == "Rock" && computerChoice == "Scissors")
         || (playerChoice == "Paper" && computerChoice == "Rock")
         || (playerChoice == "Scissors" && computerChoice == "Paper")) {
-            return 1;
+            scoreUser += 1;
+            return ("You won " + playerChoice + " beats " + computerChoice + "!");
     }
     
-    if ((playerChoice == "Paper" && computerChoice == "Scissors")
+    else if ((playerChoice == "Paper" && computerChoice == "Scissors")
         || (playerChoice == "Scissors" && computerChoice == "Rock")
         || (playerChoice == "Rock" && computerChoice == "Paper")) {
-            return -1;
+            scoreComp += 1;
+            return ("You lost " + computerChoice + " beats " + playerChoice + "!");
     }
 
-    else {
-        return null;
+    else{
+        return ("You both chose " + computerChoice + " and tied!");
     }
 }
 
-function game(){
-    let inputArray = getUserInput();
-    if (inputArray[0] == false){
-        console.log("Please choose Rock, Paper or Scissors");
-        inputArray = getUserInput();
-    }
-
+function game(playerChoice){
     let computerChoice = getComputerChoice();
 
-    console.log("User chose " + inputArray[1]);
+    console.log("User chose " + playerChoice);
     console.log("Computer chose " + computerChoice);
 
-    let result = startRound(inputArray[1], computerChoice)
-    printResult(result, inputArray[1], computerChoice);
+    let result = startRound(playerChoice, computerChoice);
+    console.log(scoreUser);
+    console.log(scoreComp);
 
-    score = score + result;
+    resultText.textContent = 'User chose ' + playerChoice + '...';
+    resultText.textContent = 'Computer chose ' + computerChoice + '...';
+    resultText.textContent = result;
+
+    score.textContent = (scoreUser + ' - ' + scoreComp);
     console.log("");
 }
 
 let playerChoice;
 const rockButton = document.querySelector('.rock');
+const paperButton = document.querySelector('.paper');
+const scissorsButton = document.querySelector('.scissors');
+
+const resultText = document.querySelector('.result-text');
+const score = document.querySelector('.score');
+
+console.log(score);
 rockButton.addEventListener('click', () => {
     playerChoice = "Rock";
+    game(playerChoice);
+  });
+paperButton.addEventListener('click', () => {
+    playerChoice = "Paper";
+    game(playerChoice);
+  });
+scissorsButton.addEventListener('click', () => {
+    playerChoice = "Scissors";
+    game(playerChoice);
   });
 
 
